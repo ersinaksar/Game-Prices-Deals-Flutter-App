c2597c24244b9ea5d69b9ca7498d71e4094a7b2e
ersinaksar <ersinaksar100@gmail.com>
2021-08-26 22:13:21 +0300

    repository sınıfı get_it paketi (locator)

--- lib/blocs/game_bloc.dart
+++ lib/blocs/game_bloc.dart
@@ -2,22 +2,25 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:flutter/material.dart';
-import 'package:game_prices_deals_flutter_app/models/game_model.dart';
+import 'package:game_prices_deals_flutter_app/data_app/game_repository.dart';
+import 'package:game_prices_deals_flutter_app/locator.dart';
+import 'package:game_prices_deals_flutter_app/models_app/game_model.dart';
 
 part 'game_event.dart';
 part 'game_state.dart';
 
 class GameBloc extends Bloc<GameEvent, GameState> {
   GameBloc() : super(GameInitial());
-
+  GameRepository gameRepository = locator<GameRepository>();
   @override
   Stream<GameState> mapEventToState(GameEvent event) async* {
     // TODO: implement mapEventToState
     if (event is FetchGameEvent) {
       yield GameLoadingState();
       try {
-        yield GameLoadedState(gameModel: GameModel());
+        final GameModel? handedGame =
+            await gameRepository.getGame(event.game_name);
+        yield GameLoadedState(gameModel: handedGame!);
       } catch (e) {
         GameErrorState();
       }
---
+++ lib/data_app/game_repository.dart
@@ -0,0 +1,7 @@
+import 'package:game_prices_deals_flutter_app/models_app/game_model.dart';
+
+class GameRepository {
+  Future<GameModel?> getGame(String game) async {
+    return null;
+  }
+}
